151dcbf1b41d16fc5fe3767712e62d0d54450514
Matt Watson <mattwat@gmail.com>
2020-09-14 13:48:01 -0400

    style tweaks

--- src/components/typography/heading.js
+++ src/components/typography/heading.js
@@ -2,19 +2,13 @@ import styled from 'styled-components'
 
 export const Heading = styled.h2(({ theme }) => `
     text-align: ${ ({ center }) => center ? 'center' : 'left' };
-    font-size: 1.75rem;
-    @media (min-width: 600px) {
-        font-size: 3.0vw;
-    }
-    @media (min-width: 992px) {
-        font-size: 1.33rem;
-    }
+    font-size: clamp(1.5rem, 3vw, 1.75rem);
     color: ${ theme.color.darkgrey };
     line-height: 1.5;
 `)
 
 export const Subheading = styled.h4`
-    font-size: 175%;
+    font-size: clamp(1.33rem, 2.75vw, 1.5rem);
     padding: 0.25rem 0;
     margin: 0;
     text-align: ${ ({ center }) => center ? 'center' : 'left' };
--- src/components/typography/title.js
+++ src/components/typography/title.js
@@ -2,13 +2,7 @@ import styled from 'styled-components'
 
 export const Title = styled.h1(({ theme, center }) => `
     text-align: ${ center ? 'center' : 'left' };
-    font-size: 2.0rem;
-    @media (min-width: 600px) {
-        font-size: 6vw;
-    }
-    @media (min-width: 992px) {
-        font-size: 3.25rem;
-    }
+    font-size: clamp(2rem, 6vw, 3.5rem);
     color: ${ theme.color.darkgrey };
     padding: 0.25rem 0.5rem;
     padding: 0;
