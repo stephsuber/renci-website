ece0fb5f6aba94224e375f5f2ea39966f66031bd
Matt Watson <mattwat@gmail.com>
2020-11-25 10:00:53 -0500

    reduce hr width

--- src/components/layout/horizontal-rule.js
+++ src/components/layout/horizontal-rule.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const HorizontalRule = styled.hr(({ theme }) => `
     border: 0;
-    height: 2px;
+    height: 1px;
     margin: 1rem auto;
     width: 100%;
     background: ${ theme.color.lightgrey };
