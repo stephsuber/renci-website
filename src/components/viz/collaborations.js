c0efd00e66814bd602db0f349f1ccb33b837951b
Matt Watson <mattwat@gmail.com>
2020-12-02 00:31:00 -0500

    clean up code

--- src/components/viz/collaborations.js
+++ src/components/viz/collaborations.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import styled, { useTheme } from 'styled-components'
 import { graphql, useStaticQuery } from 'gatsby'
 import loadable from '@loadable/component'
@@ -21,38 +21,49 @@ export const CollaborationsNetwork = () => {
   const theme = useTheme()
   const data = useStaticQuery(collaborationsQuery)
   const [networkData, setNetworkData] = useState({ nodes: [], edges: [] })
+  const groups = data.groups.edges.map(({ node }) => node)
+  const collaborations = data.collaborations.edges.map(({ node }) => node)
 
-  const groupNode = (id, name) => ({
+  // const [selectedNodes, setSelectedNodes] = useState([])
+
+  const groupNode = useCallback((id, name) => ({
     type: 'group',
     id: id,
     name: name,
     color: theme.color.renciBlue,
     val: 20,
-  })
+  }), [theme.color])
 
-  const collaborationNode = (id, name) => ({
+  const collaborationNode = useCallback((id, name) => ({
     type: 'collaboration',
     id: id,
     name: name,
     color: theme.color.carolinaBlue,
     val: 20,
-  })
+  }), [theme.color])
 
-  const projectNode = (id, name) => ({
+  const projectNode = useCallback((id, name) => ({
     type: 'project',
     id: id,
     name: name,
     color: theme.color.extended.contessa,
     val: 10,
-  })
+  }), [theme.color])
 
-  const organizationNode = (id, name) => ({
+  const organizationNode = useCallback((id, name) => ({
     type: 'organization',
     id: id,
     name: name,
     color: theme.color.extended.sherbet,
     val: 5,
-  })
+  }), [theme.color])
+
+  const neighbors = useCallback(node => {
+    const neighborhood = []
+    const incidentEdges = networkData.links.filter(({ source, target }) => source.id === node.id || target.id === node.id)
+    console.log(incidentEdges)
+    return neighborhood
+  }, [networkData])
 
   useEffect(() => console.log(networkData), [networkData])
 
@@ -60,93 +71,56 @@ export const CollaborationsNetwork = () => {
     let nodes = []
     let edges = []
 
-    const groups = data.groups.edges.map(({ node }) => node)
-    const collaborations = data.collaborations.edges.map(({ node }) => node)
-
     // add group nodes
     groups.forEach(group => nodes.push(groupNode(group.id, group.name)))
     // add collaboration nodes
     collaborations.forEach(collaboration => nodes.push(collaborationNode(collaboration.id, collaboration.name)))
 
     groups.concat(collaborations).forEach(group => {
-      console.log(group)
       if (group.partners) {
         group.partners.forEach(partner => {
           // add partner node
-          const nodeIndex = nodes.findIndex(node => node.id === partner.id)
-          if (nodeIndex === -1) {
-            nodes.push(organizationNode(partner.id, partner.name))
-          }
+          nodes.findIndex(node => node.id === partner.id) === -1 && nodes.push(organizationNode(partner.id, partner.name))
           // add project--partner edges
-          const edgeIndex = edges.findIndex(edge => edge.source === group.id && edge.target === partner.id)
-          if (edgeIndex === -1) {
-            edges.push({ source: group.id, target: partner.id, value: 10 })
-          }
+          edges.findIndex(edge => edge.source === group.id && edge.target === partner.id) === -1 && edges.push({ source: group.id, target: partner.id, value: 10 })
         })
       }
 
       if (group.funding) {
         group.funding.forEach(funder => {
           // add funder node
-          const nodeIndex = nodes.findIndex(node => node.id === funder.id)
-          if (nodeIndex === -1) {
-            nodes.push(organizationNode(funder.id, funder.name))
-          }
+          nodes.findIndex(node => node.id === funder.id) === -1 && nodes.push(organizationNode(funder.id, funder.name))
           // add project--funder edges
-          const edgeIndex = edges.findIndex(edge => edge.source === group.id && edge.target === funder.id)
-          if (edgeIndex === -1) {
-            edges.push({ source: group.id, target: funder.id, value: 15 })
-          }
+          edges.findIndex(edge => edge.source === group.id && edge.target === funder.id) === -1 && edges.push({ source: group.id, target: funder.id, value: 15 })
         })
       }
       if (group.projects) {
         group.projects.forEach(project => {
           // add project node
-          const nodeIndex = nodes.findIndex(node => node.id === project.id)
-          if (nodeIndex === -1) {
-            nodes.push(projectNode(project.id, project.name))
-          }
-
+          nodes.findIndex(node => node.id === project.id) === -1 && nodes.push(projectNode(project.id, project.name))
           // add group--project edges
-          const edgeIndex = edges.findIndex(edge => edge.source === group.id && edge.target === project.id)
-          if (edgeIndex === -1) {
-            edges.push({ source: group.id, target: project.id, value: 5 })
-          }
+          edges.findIndex(edge => edge.source === group.id && edge.target === project.id) === -1 && edges.push({ source: group.id, target: project.id, value: 5 })
 
           if (project.partners) {
             project.partners.forEach(partner => {
               // add partner node
-              const nodeIndex = nodes.findIndex(node => node.id === partner.id)
-              if (nodeIndex === -1) {
-                nodes.push(organizationNode(partner.id, partner.name))
-              }
+              nodes.findIndex(node => node.id === partner.id) === -1 && nodes.push(organizationNode(partner.id, partner.name))
               // add project--partner edges
-              const edgeIndex = edges.findIndex(edge => edge.source === project.id && edge.target === partner.id)
-              if (edgeIndex === -1) {
-                edges.push({ source: project.id, target: partner.id, value: 10 })
-              }
+              edges.findIndex(edge => edge.source === project.id && edge.target === partner.id) === -1 && edges.push({ source: project.id, target: partner.id, value: 10 })
             })
           }
 
           if (project.funding) {
             project.funding.forEach(funder => {
               // add funder node
-              const nodeIndex = nodes.findIndex(node => node.id === funder.id)
-              if (nodeIndex === -1) {
-                nodes.push(organizationNode(funder.id, funder.name))
-              }
+              nodes.findIndex(node => node.id === funder.id) === -1 && nodes.push(organizationNode(funder.id, funder.name))
               // add project--funder edges
-              const edgeIndex = edges.findIndex(edge => edge.source === project.id && edge.target === funder.id)
-              if (edgeIndex === -1) {
-                edges.push({ source: project.id, target: funder.id, value: 15 })
-              }
+              edges.findIndex(edge => edge.source === project.id && edge.target === funder.id) === -1 && edges.push({ source: project.id, target: funder.id, value: 15 })
             })
           }
         })
       }
     })
-    console.log(nodes)
-    console.log(edges)
     setNetworkData({ nodes: nodes, links: edges })
   }, [])
 
@@ -164,7 +138,8 @@ export const CollaborationsNetwork = () => {
             nodeColor={ node => node.color }
             onNodeClick={
               node => {
-                console.log(node)
+                console.log(node.id, node.name)
+                console.log(neighbors(node))
               }
             }
           />
