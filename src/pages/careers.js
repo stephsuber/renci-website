6b6fc3f18a974cba5892d00ec7ae65a128b809b9
Matt Watson <mattwat@gmail.com>
2020-12-03 17:12:53 -0500

    page formatting

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { SEO } from '../components/seo'
-import { Container, Section } from '../components/layout'
+import { Container, Hero, Section, Article } from '../components/layout'
 import { Title, Paragraph } from '../components/typography'
 import { useCareers } from '../hooks'
 import { JobPosting } from '../components/careers'
@@ -9,33 +9,44 @@ const CareersPage = () => {
   const careers = useCareers()
   
   return (
-    <Container>
-      <SEO title="RENCI Careers" />
-      
-      <Title>Careers</Title>
+    <Fragment>
+      <Hero>
+        <Title>RENCI Careers</Title>
+      </Hero>
+      <Container>
+        <SEO title="RENCI Careers" />
 
-      <Paragraph>
-        RENCI's inclusive, creative ethod transcende disciplinary boundaries to catalyze data-driven discovery and innovation for the public good.
-        A career at RENCI offers a stimulating, collaborative environment where you can unleash your intellectual potential to advance technology and accelereate research.
-        We look forward to meeting you!
-      </Paragraph>
+        <Section title="Why RENCI?">
+          <Article title="Because it's Cool">
+            <Paragraph>
+              RENCI's inclusive, creative method transcends disciplinary boundaries to catalyze data-driven discovery and innovation for the public good.
+              A career at RENCI offers a stimulating, collaborative environment where you can unleash your intellectual potential to advance technology and accelereate research.
+              We look forward to meeting you!
+            </Paragraph>
+            <Paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam provident magnam similique assumenda, corporis vel nemo optio eius aut asperiores ducimus hic in eligendi eaque nesciunt quas eos dignissimos aliquam.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam provident magnam similique assumenda, corporis vel nemo optio eius aut asperiores ducimus hic in eligendi eaque nesciunt quas eos dignissimos aliquam.
+            </Paragraph>
+          </Article>
+        </Section>
 
-      <Section title="Open Positions">
-        {
-          careers.map(posting => (
-            <JobPosting
-              key={ posting.id }
-              title={ posting.frontmatter.title }
-              publishDate={ posting.frontmatter.publish_date }
-              positionNumber={ posting.frontmatter.unc_position_number }
-              description={ posting.html }
-              url={ `http://unc.peopleadmin.com/postings/${ posting.frontmatter.unc_position_number }` }
-            />
-          ))
-        }
-      </Section>
+        <Section title="Open Positions">
+          {
+            careers.map(posting => (
+              <JobPosting
+                key={ posting.id }
+                title={ posting.frontmatter.title }
+                publishDate={ posting.frontmatter.publish_date }
+                positionNumber={ posting.frontmatter.unc_position_number }
+                description={ posting.html }
+                url={ `http://unc.peopleadmin.com/postings/${ posting.frontmatter.unc_position_number }` }
+              />
+            ))
+          }
+        </Section>
 
-    </Container>
+      </Container>
+    </Fragment>
   )
 }
 
