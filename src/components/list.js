0f2968a300889e73ae28a16c74e45566a664fbd1
Matt Watson <mattwat@gmail.com>
2020-12-03 20:31:45 -0500

    reduce list top margin

--- src/components/list.js
+++ src/components/list.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.ul(({ theme, bullets }) => `
   padding: ${ bullets === 'none' ? 0 : '0 0 0 1rem' };
-  margin: ${ theme.spacing.medium } 0;
+  margin: ${ theme.spacing.small } 0;
   list-style-type: ${ bullets };
 `)
 
