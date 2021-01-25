4247dd6023bbb1924aba8f41df2ecd46151fa10f
Matt Watson <mattwat@gmail.com>
2021-01-25 12:18:02 -0500

    tweak arrow-link component

--- src/components/link/arrow-link.js
+++ src/components/link/arrow-link.js
@@ -3,28 +3,29 @@ import styled from 'styled-components'
 import { Link } from 'gatsby'
 import { Icon } from '../icon'
 
-const Wrapper = styled(Link)(({ theme, float }) => `
-    display: inline-flex;
-    align-items: center;
-    float: ${ float ? float : 'none' };
-    & .arrow-right {
-        fill: ${ theme.color.darkgrey };
-        margin-left: 0.1rem;
-        transition: transform 100ms, opacity 250ms;
-        transform: translateX(0rem);
-        opacity: 0.5;
+const Wrapper = styled.span(({ theme, float }) => `
+  float: ${ float ? float : 'none' };
+  & .icon {
+    fill: ${ theme.color.darkgrey };
+    transition: transform 100ms, opacity 250ms;
+    transform: translateX(0rem) scale(0.9) translate(0.2rem);
+    transform-origin: 0% 50%;
+    opacity: 0.25;
+  }
+  &:hover, &:focus {
+    transition: transform 250ms, opacity 100ms;
+    & .icon {
+      transform: translateX(0.1rem) scale(1.0) translate(0.3rem);
+      opacity: 1.0;
     }
-    &:hover, &:focus {
-        transition: transform 250ms, opacity 100ms;
-        & .arrow-right {
-            transform: translateX(0.1rem);
-            opacity: 1.0;
-        }
-    }
+  }
 `)
 
 export const ArrowLink = ({ text, ...props }) => (
-    <Wrapper { ...props }>
-        { text } <Icon icon="arrow-right" className="arrow-right" size={ 14 } />
-    </Wrapper>
+  <Wrapper>
+    <Link { ...props }>
+      { text }
+    </Link>
+    <Icon icon="arrow-right" className="icon" size={ 14 } />
+  </Wrapper>
 )
