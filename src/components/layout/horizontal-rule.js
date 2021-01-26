f43b67f1b08004a6a341ccb3c65f9f0d7f0aba4c
Matt Watson <mattwat@gmail.com>
2021-01-26 10:14:18 -0500

    reduce indentation

--- src/components/layout/horizontal-rule.js
+++ src/components/layout/horizontal-rule.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components'
 
 export const HorizontalRule = styled.hr(({ theme }) => `
-    border: 0;
-    height: 1px;
-    margin: 1rem auto;
-    width: 100%;
-    background: ${ theme.color.lightgrey };
+  border: 0;
+  height: 1px;
+  margin: ${ theme.spacing.medium } auto;
+  width: 100%;
+  background: ${ theme.color.lightgrey };
 `)
